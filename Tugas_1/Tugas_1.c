4e6092fd58bc5f51f70572c8f2a7694f7a889732
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-11 16:48:08 +0700

    buat program sederhana itung jam kerja karyawan

---
+++ Tugas_1/Tugas_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   char Str[500];
+   int Persentase;
+   int jmlhJamKerja;
+
+   printf ("PROGRAM HINTUNG GAJI KARYAWAN\n");
+   printf ("Nama karyawan:... \n");
+   scanf("%[^\n]", Str);
+   printf("Golongan Jabatan:... \n");
+   scanf("%d", &Persentase);
+   printf ("Pendidikan:... \n");
+   scanf("%d", &Persentase);
+   printf ("Jumlah jam kerja:... \n");
+   scanf("%d", &jmlhJamKerja);
+
+   
+}
