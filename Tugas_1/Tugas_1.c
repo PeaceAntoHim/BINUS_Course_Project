76d0c8ebdb57478a0cc39aff47c265ef29af3c0d
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-12 17:32:42 +0700

    ubah lagi

--- Tugas_1/Tugas_1.c
+++ Tugas_1/Tugas_1.c
@@ -20,22 +20,30 @@ int main() {
 
    // Nama Karyawab
    printf("Nama Karyawan: %s\n", nama);
+
+   // Persentase Jabatan
    if ( persentaseJ == 1 ) {
-      gaji1 = (2000000 * 00.5);
+      gaji1 = (2000000 * 0.05);
    } else if ( persentaseJ == 2 ) {
-      gaji1 = (2000000 * 0.1);
+      gaji1 = (2000000 * 0.10);
    } else if ( persentaseJ == 3 ) {
       gaji1 = (2000000 * 0.15);
+   } else {
+      printf("Golongan Jabatan tidak ditemukan\n");
+      return 0;
    }
    printf("    Tunjangan Jabatan: Rp. %d\n", gaji1);
 
    // Tunjagnan pendidikan
    if ( persentaseP == SMA) {
-      gaji2 = (2000000 * 00.25);
+      gaji2 = (2000000 * 0.025);
    } else if ( persentaseP == D3) {
-      gaji2 = (2000000 * 00.5);
+      gaji2 = (2000000 * 0.05);
    } else if ( persentaseP == S1) {
-      gaji2 = (2000000 * 0.1);
+      gaji2 = (2000000 * 0.10);
+   } else {
+      printf("Pendidikan tidak ditemukan\n");
+      return 0;
    }
    printf ("    Tunjangan Pendidikan: RP. %d\n", gaji2);
 
@@ -46,8 +54,11 @@ int main() {
       gaji3 = (2000000 - ((8 - jmlhJamKerja) * 3000));
    } else if( jmlhJamKerja > 8) {
       gaji3 = (20000000 + ((jmlhJamKerja - 8) * 3000));
+   } else {
+      printf("Jumlah jam kerja tidak ditemukan\n");
+      return 0;
    }
-   printf ("    Honor Lembur: Rp.%d\n", gaji3);
+   printf ("    Honor Lembur: Rp. %d\n", gaji3);
    // Total gaji
    printf("Total Gaji: Rp. %d\n", (gaji1 + gaji2 + gaji3));
    return 0;
