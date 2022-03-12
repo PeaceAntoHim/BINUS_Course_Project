e6a50b5e6dc89093d81abfb4d1ead0c4824554d8
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-12 18:19:40 +0700

    nambaih fitur

--- Tugas_1/Tugas_1.c
+++ Tugas_1/Tugas_1.c
@@ -2,11 +2,12 @@
 
 int main() {
    // Deklarasi variabel
-   char nama[125];
-   int persentaseJ, persentaseP, jmlhJamKerja, gaji1, gaji2, gaji3;
-   int SMA = 1;
    int D3 = 2;
    int S1 = 3;
+   int SMA = 1;
+   char nama[125];
+   int gajiPerBulan = 2000000;
+   int persentaseJ, persentaseP, jmlhJamKerja, gaji1, gaji2, gaji3;
 
    printf ("PROGRAM HINTUNG GAJI KARYAWAN\n\n");
    printf ("Nama karyawan: ");
@@ -20,13 +21,14 @@ int main() {
 
    // Nama Karyawab
    printf("Nama Karyawan: %s\n", nama);
+   printf("    Gaji per bulan: Rp. %d\n", gajiPerBulan);
 
    // Persentase Jabatan
-   if ( persentaseJ == 1 ) {
+   if (persentaseJ == 1 ) {
       gaji1 = (2000000 * 0.05);
-   } else if ( persentaseJ == 2 ) {
+   } else if (persentaseJ == 2 ) {
       gaji1 = (2000000 * 0.10);
-   } else if ( persentaseJ == 3 ) {
+   } else if (persentaseJ == 3 ) {
       gaji1 = (2000000 * 0.15);
    } else {
       printf("Golongan Jabatan tidak ditemukan\n");
@@ -35,32 +37,30 @@ int main() {
    printf("    Tunjangan Jabatan: Rp. %d\n", gaji1);
 
    // Tunjagnan pendidikan
-   if ( persentaseP == SMA) {
+   if (persentaseP == SMA) {
       gaji2 = (2000000 * 0.025);
-   } else if ( persentaseP == D3) {
+   } else if (persentaseP == D3) {
       gaji2 = (2000000 * 0.05);
-   } else if ( persentaseP == S1) {
+   } else if (persentaseP == S1) {
       gaji2 = (2000000 * 0.10);
    } else {
-      printf("Pendidikan tidak ditemukan\n");
+      printf("Gologan pendidikan tidak ditemukan mohon masukan salah satu kode ini SMA = 1, D3 = 2, S1 = 3\n");
       return 0;
    }
-   printf ("    Tunjangan Pendidikan: RP. %d\n", gaji2);
+   printf("    Tunjangan Pendidikan: RP. %d\n", gaji2);
 
    // Hitung lama lembur
-   if( jmlhJamKerja == 8) {
-      gaji3 = (2000000 * 1);
-   } else if( jmlhJamKerja < 8) {
-      gaji3 = (2000000 - ((8 - jmlhJamKerja) * 3000));
-   } else if( jmlhJamKerja > 8) {
-      gaji3 = (20000000 + ((jmlhJamKerja - 8) * 3000));
+   if(jmlhJamKerja > 8) {
+      gaji3 = ((jmlhJamKerja - 8) * 3000);
+   } else if(jmlhJamKerja == 8) {
+      gaji3 = (jmlhJamKerja * 0);
    } else {
-      printf("Jumlah jam kerja tidak ditemukan\n");
+      printf("Jumlah jam kerja anda kurang dari 8 jam\n");
       return 0;
    }
    printf ("    Honor Lembur: Rp. %d\n", gaji3);
    // Total gaji
-   printf("Total Gaji: Rp. %d\n", (gaji1 + gaji2 + gaji3));
+   printf("Total Gaji: Rp. %d\n", (gajiPerBulan + gaji1 + gaji2 + gaji3));
    return 0;
 }
 
