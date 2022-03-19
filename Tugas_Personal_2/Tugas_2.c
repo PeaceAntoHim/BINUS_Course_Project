503e1042ac6c81732eae17d66ba6cf3bf206e0bc
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-19 11:29:08 +0700

    nambah scema data

--- Tugas_Personal_2/Tugas_2.c
+++ Tugas_Personal_2/Tugas_2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//Struktur data inputan biodata mahasiswa
 struct mhs {
    char  nim[11];
    char  nama[50];
@@ -8,16 +9,26 @@ struct mhs {
    char mataKuliah[50];
 };
 
+//Struktur data inputan nilai mahasiswa
+struct nilai {
+	int nilaiHadir;
+	int nilaiTugas;
+	int nilaiQuiz;
+	int nilaiForum;
+	int nilaiUas;
+};
+
+
 int main () {
    struct mhs info; 
    float wgpa;
 
    printf("Masukan Nim anda = "); scanf("%s", &info.nim);
    fflush(stdin);
-   printf("Masukan Nama anda = "); scanf("%s", &info.nama);
+   printf("Masukan Nama anda = "); scanf("[^\n]", &info.nama);
    printf("Masukan GPA anda = "); scanf("%f", &wgpa);
    info.gpa = wgpa;
-   printf("Masukan Mata Kuliah anda = "); scanf("%s", &info.mataKuliah);
+   printf("Masukan Mata Kuliah anda = "); scanf("[^\n]", &info.mataKuliah);
    printf("Tampilkan Nim = %s\n", info.nim);
    printf("Tampilkan Nama = %s\n", info.nama);
    printf("Tampilkan GPA =  %.2f\n", info.gpa);
