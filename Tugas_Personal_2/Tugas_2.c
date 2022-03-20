fa449de9ef72a344a95232d4b59da317c59b2b55
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-20 22:07:10 +0700

    got an error

--- Tugas_Personal_2/Tugas_2.c
+++ Tugas_Personal_2/Tugas_2.c
@@ -5,8 +5,8 @@
 struct mhs {
    char  nim[11];
    char  nama[50];
+   char jurusan[50];
    float gpa;
-   char mataKuliah[50];
 };
 
 //Struktur data inputan nilai mahasiswa
@@ -18,36 +18,29 @@ struct nilai {
 	int nilaiUas;
 };
 
-struct menu {
-	char Menu1;
-	char Menu2;
-	char Menu3;
-	char Menu4;
-};
 
 int main () {
    struct mhs info; 
    struct nilai data;
-   struct menu option;
+   char Menu1, Menu2, Menu3, Menu4;
    float wgpa;
    
    
-	printf("Plih menu di bawah ini \n Menu 1\n Menu 2\n"); scanf("[^\n]", &option.Menu1);
-   if (&option.Menu1 == 1) {
-      printf("Masukan Nim anda = "); scanf("%s", &info.nim);
+   
+	printf("Plih menu di bawah ini \n Menu 1\n Menu 2\n"); scanf("[^\n]", Menu1);
+   if (Menu1 = '1') {
+      printf("Masukan Nim anda = "); scanf("%[^\n]", &info.nim);
       fflush(stdin);
-      printf("Masukan Nama anda = "); scanf("[^\n]", &info.nama);
+      printf("Masukan Nama anda = "); scanf("%[^\n]", &info.nama);
       printf("Masukan GPA anda = "); scanf("%f", &wgpa);
-      info.gpa = wgpa;
-      printf("Masukan Mata Kuliah anda = "); scanf("[^\n]", &info.mataKuliah);
-   } else if() {
-   	
-   } else if(&option.Menu3 == 3) {
-   	  printf("Tampilkan Nim = %s\n", info.nim);
-      printf("Tampilkan Nama = %s\n", info.nama);
-      printf("Tampilkan GPA =  %.2f\n", info.gpa);
-      printf("Tampilkan mata kuliah = %s\n", info.mataKuliah);
-   }   
+	  info.gpa = wgpa;  
+      printf("Masukan jurusan anda = "); scanf("%[^\n]", &info.jurusan);
+   }  
+   
+   	printf("Tampilkan Nim = %s\n", info.nim);
+    printf("Tampilkan Nama = %s\n", info.nama);
+    printf("Tampilkan GPA =  %.2f\n", info.gpa);
+    printf("Tampilkan jurusan = %s\n", info.jurusan);
    
    return 0;
 }
