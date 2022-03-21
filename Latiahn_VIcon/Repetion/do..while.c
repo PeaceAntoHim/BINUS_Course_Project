6e6227d5a7296a69c2ba3255dcab46ed071c6bd8
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-21 21:23:40 +0700

    buat do..while

---
+++ Latiahn_VIcon/Repetion/do..while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+// deklarasi valriabel 
+int i = 20;
+
+// Perulangan menggunakan do while 
+do {
+printf("Nilai dari variable i: %d\n", i);
+i = i + 1;
+} while( i <= 40 );
+
+return 0;
+}
---
+++ Latiahn_VIcon/Repetion/while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+int i = 1;
+int N = 8;
+
+while(i <= 10) {
+printf("\n %i", N);
+N = N + 8;
+i=i+1;
+}
+
+}
