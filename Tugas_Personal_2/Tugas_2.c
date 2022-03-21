579832dad5890c51992f3ed59853eb298372b55b
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-21 22:55:58 +0700

    buat program 1 lagi

--- Tugas_Personal_2/Tugas_2.c
+++ Tugas_Personal_2/Tugas_2.c
@@ -23,27 +23,32 @@ int main () {
    struct mhs info; 
    struct nilai data;
    char Menu1[10], Menu2[10], Menu3[10], Menu4[10];
+   char szKey1[] = "Menu 1";
+   char szInput1[80];
    float wgpa;
    
 	printf("Plih menu di bawah ini \n\n Menu 1 untuk menginput biodata\n Menu 2 untuk menginput nilai\n Menu 3 untuk melihat nilai mahasiswa dan status\n Menu 4 untuk keluar dari program\n\n"); 
-	scanf("%[^\n]", &Menu1, &Menu2, &Menu3, &Menu4);
+	scanf("%[^\n]", &Menu1, &Menu2);
 	
-   if (Menu1=="Menu 1") {
+   if ( strcmp(Menu1, "Menu 1") == 0) {
    		fflush(stdin);	
-	    printf("Masukan Nim anda = "); scanf("%[^\n]", &info.nim);
+	    printf("Masukan Nim Anda = "); scanf("%[^\n]", &info.nim);
 	    fflush(stdin);
-	    printf("Masukan Nama anda = "); scanf("%[^\n]", &info.nama);
-	    printf("Masukan GPA anda = "); scanf("%f", &wgpa);
+	    printf("Masukan Nama Anda = "); scanf("%[^\n]", &info.nama);
+	    printf("Masukan GPA Anda = "); scanf("%f", &wgpa);
 		info.gpa = wgpa;
 		fflush(stdin);  
-	    printf("Masukan jurusan anda = "); scanf("%[^\n]", &info.jurusan);
-    } else if(Menu2=="Menu 2") {
-	  	printf("test");
+	    printf("Masukan Jurusan Anda = "); scanf("%[^\n]", &info.jurusan);
+    } else if( strcmp(Menu1, "Menu 2") == 0 ) {
+		printf("Masukan Hilai Hadir Anda = "); scanf("%d", &data.nilaiHadir);
+		printf("Masukan Nilai Tugas Anda = "); scanf("%d", &data.nilaiTugas);
+		printf("Masukan Nilai Quiz Anda = "); scanf("%d", &data.nilaiQuiz);
+		printf("Masukan Nilai Keaktifan Forum Anda = "); scanf("%d", &data.nilaiUas); 	
 	}  else {
 		printf("error");
 	}
    
-//   	printf("Tampilkan Nim = %s\n", info.nim);
+//    printf("Tampilkan Nim = %s\n", info.nim);
 //    printf("Tampilkan Nama = %s\n", info.nama);
 //    printf("Tampilkan GPA =  %.2f\n", info.gpa);
 //    printf("Tampilkan jurusan = %s\n", info.jurusan);
