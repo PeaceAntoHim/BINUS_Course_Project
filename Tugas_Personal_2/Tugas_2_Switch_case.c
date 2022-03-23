7fa3dcaa298e9312a21828549de8a0947ac1edcc
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-23 23:30:10 +0700

    tambahin kalo pake switdch case

--- Tugas_Personal_2/Tugas_2_Switch_case.c
+++ Tugas_Personal_2/Tugas_2_Switch_case.c
@@ -22,19 +22,19 @@ struct nilai {
 int main () {
    struct mhs info; 
    struct nilai data;
-   char Menu1[10];
+   char Menu[10];
    char szKey1[] = "Menu 1";
    char szInput1[80];
    float wgpa;
    int nilaiHadir, nilaiTugas, nilaiForum, nilaiQuiz, nilaiUas;
    
 	printf("Plih menu di bawah ini \n\n Menu 1 untuk menginput biodata\n Menu 2 untuk menginput nilai\n Menu 3 untuk melihat nilai mahasiswa dan status\n Menu 4 untuk keluar dari program\n\n"); 
-	scanf("%[^\n]", &Menu1);
-	int loop = 1;
-	while(loop) {
-		
-	   if ( strcmp(Menu1, "Menu 1") == 0) {
-	   		fflush(stdin);	
+	scanf("%[^\n]", &Menu);
+
+		switch(Menu) {
+		    case '1' :
+		    {
+		    fflush(stdin);	
 		    printf("Masukan Nim Anda = "); scanf("%[^\n]", &info.nim);
 		    fflush(stdin);
 		    printf("Masukan Nama Anda = "); scanf("%[^\n]", &info.nama);
@@ -42,8 +42,10 @@ int main () {
 			info.gpa = wgpa;
 			fflush(stdin);  
 		    printf("Masukan Jurusan Anda = "); scanf("%[^\n]", &info.jurusan);
-	    } else if( strcmp(Menu1, "Menu 2") == 0 ) {
-			printf("Masukan Hilai Hadir Anda = "); scanf("%d", &data.nilaiHadir);
+		    }
+		    case '2' :
+		    {
+		    printf("Masukan Hilai Hadir Anda = "); scanf("%d", &data.nilaiHadir);
 			nilaiHadir = (data.nilaiHadir * 0.1);
 			printf("Masukan Nilai Tugas Anda = "); scanf("%d", &data.nilaiTugas);
 			nilaiTugas = (data.nilaiTugas * 0.2);
@@ -53,22 +55,21 @@ int main () {
 			nilaiForum = (data.nilaiForum * 0.1);
 			printf("Masukan Nilai Uas Anda = "); scanf("%d", &data.nilaiUas);
 			nilaiUas = (data.nilaiUas * 0.5);
-			break;
-		} else if( strcmp(Menu1, "Menu 3") == 0)  {
-			fflush(stdin);
+		    }
+		    case '3':
+		    {
+		    fflush(stdin);
 			printf("Masukan NIM anda untuk melihat kalkulasi nilai = "); scanf("%[^\n]", &info.nim);
-			if(info.nim == info.nim) {
-				printf("Hasil kalkulasi = ");
-				printf("%d", (nilaiHadir + nilaiTugas + nilaiForum + nilaiQuiz + nilaiUas));
-			}
-			break;
-		} else if( strcmp(Menu1, "Menu 4") == 0 ) {
-			printf("Anda sudah keluar dari program. Terima Kasih sudah menggunakan program saya, Semoga membantu");
-			return 0;	
-		} else {
-			printf("Mohon masukan menu dengan benar hanya terdiri dari (Menu 1, Menu 2, Menu 3 dan Menu 4).");
-		}
-   return 0;
+    			if(info.nim == info.nim) {
+    				printf("Hasil kalkulasi = ");
+    				printf("%d", (nilaiHadir + nilaiTugas + nilaiForum + nilaiQuiz + nilaiUas));
+    			}    
+		    }
+		    case '4':
+		    {
+		  printf("Anda sudah keluar dari program. Terima Kasih sudah menggunakan program saya, Semoga membantu");
+			return 0;	 
+		    }
 	}
 
    
