a08c4837625491b4d41688d407c3872e5fde63e3
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-12-05 03:06:18 +0700

    created new

--- Data_Structure/linkedlist/SingleLinkedlist.c
+++ Data_Structure/linkedlist/SingleLinkedlist.c
@@ -1,148 +1,140 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-struct stokBarang
-{
-   char namaBarang[51];
-   char jenisBarang[21];
-   int jumlahBarang;
-   int hargaBarang;
-   struct stokBarang *next;
-} * head, *tail, *curr;
+// Created SingleLinkedList
 
-void view()
+struct Data
 {
-   int i = 0, j = 0;
-   printf("        --------------------- LIST BARANG -------------- \n\n");
-   for (j = 0; j < 100; j++)
-      printf("=");
-   printf("\n");
-   printf("| %-3s    |   %-20s   |   %-15s   |   %-8s    |   %-8s    |\n",
-          "No.", "namaBarang", "jenisBarang", "jumlahBarang", "hargaBarang");
-   for (j = 0; j < 100; j++)
-      printf("=");
-   printf("\n");
-   curr = head;
-   while (curr)
+   int value;
+   struct Data *next;
+} * head, *tail;
+
+void pushHead(int x)
+{
+   struct Data *node = malloc(sizeof(struct Data));
+   node->value = x;
+   (*node).next = NULL;
+
+   // created validation
+   if (head == NULL)
    {
-      i++;
-      printf("| %3d.  |   %-20s   |   %-15s   |   %8d     | Rp%8d     |\n",
-             i, curr->namaBarang, curr->jenisBarang, curr->jumlahBarang, curr->hargaBarang);
-      curr = curr->next;
-   }
-   for (j = 0; j < 100; j++)
-      printf("=");
-   printf("\n");
-   getchar();
-   fflush(stdin);
-}
-
-void push(char namaBarang[], char jenisBarang[], int jumlahBarang, int hargaBarang)
-{
-   curr = (struct data *)malloc(sizeof(struct data));
-   strcpy(curr->name, name);
-   strcpy(curr->type, type);
-   curr->jumlahBarang = jumlahBarang;
-   curr->hargaBarang = hargaBarang;
-
-   if (head->NULL)
-   {
-      head = tail = curr;
+      head = tail = node;
    }
    else
    {
-      tail->next = curr;
-      tail = curr;
-   }
-   tail->next = NULL;
-}
-
-void pop(char namaBarang[])
-{
-   curr = head;
-   while (curr != NULL && strcmp(curr->namaBarang, namaBarang != 0))
-   {
-      curr = curr->next;
-   }
-
-   if (curr == NULL)
-   {
-      printf("    Nama barang yang anda masukan tidak di temukan \n");
-   }
-   else if (head == curr)
-   {
-      head = head->next;
-      free(curr);
-      printf("    Nama barang yang anda masukan telah berhasil di hapus \n");
-   }
-   else if (head != curr && tail != curr)
-   {
-      struct stokBarang *temp;
-      temp *head;
-      while (temp->next != cirr)
-         temp = temp->next;
-      temp->next = curr->next;
-      free(curr);
-      printf("    Nama barang yang anda masukan telah berhasil di hapus \n");
-   }
-   else if (tail == curr)
-   {
-      printf("    Nama barang yang anda masukan tidak bisa di hapus karena barang terkahir \n");
+      node->next = head;
+      head = node;
    }
 }
 
-void popall()
+void pushTail(int x)
 {
-   while (head != NULL)
+   struct Data *node = (struct Data *)malloc(sizeof(struct Data));
+   node->value = x;
+   node->next = NULL;
+
+   if (head == NULL)
    {
-      curr = head;
-      head = head->next;
-      free(curr);
+      head = tail = node;
+   }
+   else
+   {
+      tail->next = node;
+      tail = node;
    }
 }
 
-void menu()
+// Create popHead
+void popHead()
 {
-   printf("\n COMPUTER ADMINISTRATOR");
-   printf("\n +++++++++++++++++++++++");
-   printf("\n 1. List Barang");
-   printf("\n 2. Add (PUSH) barang baru");
-   printf("\n 3. Hapus (POP) barang lama");
-   printf("\n 4. Keluar");
-   printf("\n >> Input pilihan : ");
+   if (head != NULL)
+   {
+      if (head == tail)
+      {
+         free(head);
+         head = tail = NULL;
+      }
+      else
+      {
+         struct Data *node = head;
+         head = node->next;
+         free(node);
+         node = NULL;
+      }
+   }
 }
 
-void clear()
+// Created popTail
+void popTail()
 {
-   for (int i = 0; i < 25; i++)
-      printf("\n");
+   if (head != NULL)
+   {
+      if (head == tail)
+      {
+         free(head);
+         head = tail = NULL;
+      }
+      else
+      {
+         struct Data *curr = head;
+         while (curr->next != tail)
+         {
+            curr = curr->next;
+         }
+         tail = curr;
+         free(tail->next);
+         tail->next = NULL;
+      }
+   }
+}
+
+void print()
+{
+   if (head != NULL)
+   {
+      struct Data *curr = head;
+      while (curr != NULL)
+      {
+         printf("%d ", curr->value);
+         curr = curr->next;
+      }
+   }
+   else
+   {
+      printf("Data Kosong\n");
+   }
 }
 
 int main()
 {
-   int choice;
-   char namaBarang[51];
-   char jenisBarang[21];
-   int jumlahBarang;
-   int hargaBarang;
+   //   pushHead(30);
+   //   pushHead(10);
+   //   pushHead(40);
+   //   pushHead(20);
+   //   pushHead(50);
 
-   do
-   {
-      clear();
-      menu();
-      scanf("%d", &choice);
-      fflush(stdin);
-      switch (choice)
-      {
-      case 1:
-         view();
-         break;
-      case 2:
+   pushTail(20);
+   pushTail(10);
+   pushTail(40);
+   pushTail(30);
+   pushTail(50);
 
-         break;
-      case 3:
-         break;
-      }
-   } while (choice != 4) return 0;
+   print();
+   printf("\n");
+
+   popTail();
+   popTail();
+   popTail();
+   popTail();
+   popTail();
+
+   //   popHead();
+   //      popHead();
+   //       popHead();
+   //          popHead();
+   //           popHead();
+
+   print();
+
+   return 0;
 }
