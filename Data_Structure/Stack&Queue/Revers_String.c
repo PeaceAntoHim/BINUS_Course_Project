ff7e82a40475a940fe4b887b59c19f9089287e83
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-12-15 10:40:49 +0700

    created newe

---
+++ Data_Structure/Stack&Queue/Revers_String.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STACK_SIZE 100
+
+/* Fungsi untuk mengembalikan elemen teratas dari stack tanpa menghapusnya. */
+char peek(char *stack, int *top) {
+    return stack[*top];
+}
+
+/* Fungsi untuk menghapus elemen teratas dari stack. */
+char pop(char *stack, int *top) {
+    char elem = stack[*top];
+    (*top)--;
+    return elem;
+}
+
+/* Fungsi untuk menambahkan elemen baru ke stack. */
+void push(char *stack, int *top, char elem) {
+    (*top)++;
+    stack[*top] = elem;
+}
+
+/* Fungsi untuk membalikkan string dengan menggunakan stack. */
+char* reverse_string(char *string) {
+    int i, length, top = -1;
+    char *reversed_string, stack[STACK_SIZE];
+
+    length = strlen(string);
+    reversed_string = (char*) malloc(length + 1);
+
+    /* Pindahkan karakter-karakter dari string ke stack. */
+    for (i = 0; i < length; i++) {
+        push(stack, &top, string[i]);
+    }
+
+    /* Pop karakter-karakter dari stack dan simpan di reversed_string. */
+    for (i = 0; i < length; i++) {
+        reversed_string[i] = pop(stack, &top);
+    }
+
+    reversed_string[length] = '\0';
+    return reversed_string;
+}
+
+int main() {
+    char *string = "Mahasiswa Binus";
+    char *reversed_string = reverse_string(string);
+
+    printf("Original string: %s\n", string);
+    printf("Reversed string: %s\n", reversed_string);
+
+    free(reversed_string);
+
+    return 0;
+}
