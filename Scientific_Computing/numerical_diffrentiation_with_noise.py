e2a70ae68995210f7804a816ed17641519af34b1
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2023-05-14 23:58:30 +0700

    add new example

--- Scientific_Computing/numerical_diffrentiation_with_noise.py
+++ Scientific_Computing/numerical_diffrentiation_with_noise.py
@@ -41,3 +41,39 @@ plt.ylabel('y')
 
 plt.legend()
 plt.show()
+
+
+# define step size
+h = 1
+# define number of iterations to perform
+iterations = 20
+# list to store our step sizes
+step_size = []
+# list to store max error for each step size
+max_error = []
+
+for i in range(iterations):
+    # halve the step size
+    h /= 2
+    # store this step size
+    step_size.append(h)
+    # compute new grid
+    x = np.arange(0, 2 * np.pi, h)
+    # compute function value at grid
+    y = np.cos(x)
+    # compute vector of forward differences
+    forward_diff = np.diff(y)/h
+    # compute corresponding grid
+    x_diff = x[:-1]
+    # compute exact solution
+    exact_solution = -np.sin(x_diff)
+
+    # Compute max error between
+    # numerical derivative and exact solution
+    max_error.append(
+        max(abs(exact_solution - forward_diff)))
+
+# produce log-log plot of max error versus step size
+plt.figure(figsize=(12, 8))
+plt.loglog(step_size, max_error, 'v')
+plt.show()
