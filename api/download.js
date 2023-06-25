f3b431f72c15627a24372e7a53ce8c38c503044e
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2023-06-25 19:13:34 +0700

    fix bug

--- api/download.js
+++ api/download.js
@@ -3,7 +3,7 @@ const { join } = require("path");
 const { createReadStream } = require("fs");
 
 const server = createServer((req, res) => {
-  const filePath = join(__dirname, "..", req.url);
+  const filePath = join(__dirname, req.url);
 
   const fileStream = createReadStream(filePath);
   fileStream.pipe(res);
