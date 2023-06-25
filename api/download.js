b12f5286a4b6700474e9bda345eb5218bb022d29
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2023-06-25 19:17:06 +0700

    fix bug

--- api/download.js
+++ api/download.js
@@ -1,12 +1,28 @@
-const { createServer } = require("http");
 const { join } = require("path");
 const { createReadStream } = require("fs");
+const { promisify } = require("util");
+const { createServer } = require("http");
+const { parse } = require("url");
 
-const server = createServer((req, res) => {
-  const filePath = join(__dirname, req.url);
+const server = createServer(async (req, res) => {
+  try {
+    const { pathname } = parse(req.url);
+    const filePath = join(process.cwd(), pathname);
 
-  const fileStream = createReadStream(filePath);
-  fileStream.pipe(res);
+    const stat = await promisify(fs.stat)(filePath);
+
+    res.writeHead(200, {
+      "Content-Type": "application/octet-stream",
+      "Content-Length": stat.size,
+    });
+
+    const readStream = createReadStream(filePath);
+    readStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Internal Server Error");
+  }
 });
 
 module.exports = server;
